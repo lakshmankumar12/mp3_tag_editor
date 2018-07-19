9d01defe825559bf38c661fbe1306fbdb5a67e13
lakshman kumar <lakshmankumar@gmail.com>
2018-07-18 17:48:39 -0700

    Adding support for both saavn/pandora in classify, handle case of unknown song appearing more than once

--- classify_saavn_scrobble.py
+++ classify_saavn_scrobble.py
@@ -26,18 +26,21 @@ def addCategory(catgyFileName, newCategory, existCategories):
     existCategories.append(newCategory)
     return newCategory
 
-def addSongtoCategory(category, song):
-    fileName = os.path.join(SaavnSortedDir, category)
+def addSongtoCategory(sortedDir, category, song, songsList):
+    fileName = os.path.join(sortedDir, category)
     line = '`'.join([song['title'],song['artist'],song['album'],song['permaUrl']])
     line += '\n'
     with open (fileName, 'a') as fd:
         fd.write(line)
+    album=song['album']
+    title=song['title']
+    songsList[album][title] = category
 
-def buildAllSongsList(categories):
+def buildAllSongsList(sortedDir, categories):
     total_cnt = 0
     songsList = collections.defaultdict(dict)
     for cat in categories:
-        filename = os.path.join(SaavnSortedDir, cat)
+        filename = os.path.join(sortedDir, cat)
         try:
             with open (filename, 'r') as fd:
                 for line in fd:
@@ -58,7 +61,7 @@ def buildAllSongsList(categories):
             pass
     return (songsList, total_cnt)
 
-def askChoice(song, categories):
+def askChoice(sortedDir, song, categories, classified_songs):
     print ("What's your category for song:")
     print ("Title:  {}".format(song['title']))
     print ("Album:  {}".format(song['album']))
@@ -72,10 +75,10 @@ def askChoice(song, categories):
     ok_attempt = 1
     while ok_attempt <= 3:
         try:
-            choice = input("Choice:")
+            choice = input("Choice (Type {} to exit):".format(maxN+1))
             choice = int(choice)
-            if choice < 0 or choice > maxN:
-                print ("Enter a valid number 0 - {}".format(maxN))
+            if choice < 0 or choice > maxN+1:
+                print ("Enter a valid number 0 - {}".format(maxN+1))
                 continue
             chosen = choice
             break;
@@ -84,6 +87,8 @@ def askChoice(song, categories):
     if chosen == None:
         print ("Too many attempts")
         sys.exit(1)
+    if chosen == maxN+1:
+        return False
     chosenCategory = ""
     if choice == 0:
         ok_attempt = 1
@@ -92,7 +97,7 @@ def askChoice(song, categories):
             print ("You chose {}".format(catName))
             ok = input ("yes?")
             if ok == 'yes' or ok == 'y':
-                catFile = os.path.join(SaavnSortedDir, categoryFile)
+                catFile = os.path.join(sortedDir, categoryFile)
                 addCategory(catFile, catName, categories)
                 chosenCategory = catName
                 break;
@@ -101,7 +106,8 @@ def askChoice(song, categories):
             sys.exit(1)
     else:
         chosenCategory = categories[choice-1]
-    addSongtoCategory(chosenCategory, song)
+    addSongtoCategory(sortedDir, chosenCategory, song, classified_songs)
+    return True
 
 
 def parseScrobbleFile(scrobbleName):
@@ -124,12 +130,14 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--debug", help="print parsed files too", action="store_true")
+    parser.add_argument("--scrobbleFile", help="scrobble file", default=SaavnScrobbleFile)
+    parser.add_argument("--sortedDir", help="Sorted dir", default=SaavnSortedDir)
 
     cmd_options = parser.parse_args()
 
-    categories = getCategories(os.path.join(SaavnSortedDir,categoryFile))
-    classified_songs, total_cnt = buildAllSongsList(categories)
-    scrobbedSongs = parseScrobbleFile(SaavnScrobbleFile)
+    categories = getCategories(os.path.join(cmd_options.sortedDir,categoryFile))
+    classified_songs, total_cnt = buildAllSongsList(cmd_options.sortedDir, categories)
+    scrobbedSongs = parseScrobbleFile(cmd_options.scrobbleFile)
     new_count = 0
     for s in scrobbedSongs:
         a = s['album']
@@ -139,8 +147,11 @@ def main():
                 if cmd_options.debug:
                     print ("Found {} :{} in {}".format(t,a,classified_songs[a][t]))
                 continue
-        askChoice(s, categories)
+        carryOn = askChoice(cmd_options.sortedDir, s, categories, classified_songs)
+        if not carryOn:
+            break;
         new_count += 1
+        print ("Finished {} out of {}".format(new_count, total_cnt))
     print ("There were {} classfied songs and now you classified {} songs".format(total_cnt, new_count))
 
 
